49f9b1adf4de736f2f2318983bbf2fb60b554cbb
阿田 <5219254+artian@user.noreply.gitee.com>
2020-03-06 16:50:25 +0800

    add header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>hello world</div>
+  <home-header></home-header>
 </template>
 
 <script>
+import HomeHeader from './components/Header';
+
 export default {
   name: 'Home',
+  components: {
+    HomeHeader,
+  },
 };
 </script>
 
