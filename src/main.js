fb1af03b4385a459f094de7e5e605c9e199d87ee
阿田 <5219254+artian@user.noreply.gitee.com>
2020-03-07 17:14:29 +0800

    change

--- src/main.js
+++ src/main.js
@@ -2,14 +2,17 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import fastClick from 'fastclick';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
 import 'styles/reset.css';
 import 'styles/border.css';
 import 'styles/iconfont.css';
+import 'swiper/dist/css/swiper.css';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
 fastClick.attach(document.body);
+Vue.use(VueAwesomeSwiper);
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <swiper class="wrapper" :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data() {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1996165881,2758668698&fm=26&gp=0.jpg',
+      }, {
+        id: '002',
+        imgUrl: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2484738738,3418802711&fm=26&gp=0.jpg',
+      }],
+    };
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 200px
+    background: #eee
+    .swiper-img
+      width: 100%
+      height: 200px
+</style>
