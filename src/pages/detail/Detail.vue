af1cb4096e05eb1e1fe451620296efc7fcdd9d64
阿田 <5219254+artian@user.noreply.gitee.com>
2020-03-14 10:25:13 +0800

    fix problem

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
     <banner></banner>
+    <detail-header></detail-header>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 import Banner from '@/pages/detail/components/Banner'
+import DetailHeader from '@/pages/detail/components/Header'
 export default {
   name: 'Detail',
   components: {
     Banner,
+    DetailHeader,
   },
 }
 </script>
 
 <style lang="stylus" scoped>
+  .content
+    height: 50rem
 </style>
